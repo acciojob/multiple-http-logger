6a2673c921aa2f79d8c9588df7884067e1271539
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2023-06-30 19:14:30 +0530

    ques

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+const urls = [
+  "https://jsonplaceholder.typicode.com/posts/1",
+  "https://jsonplaceholder.typicode.com/posts/2",
+  "https://jsonplaceholder.typicode.com/posts/3",
+];
+
+// Don't change the above line
+// Write your code here
